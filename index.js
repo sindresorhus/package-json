d5a4e31cad58580c2f803764c064762d1a1da978
Hutson Betts <hutson@hyper-expanse.net>
2015-02-23 12:42:07 +0700

    Close #3 PR: feat(scope): add support. Fixes #2

--- index.js
+++ index.js
@@ -3,12 +3,14 @@ var got = require('got');
 var registryUrl = require('registry-url');
 
 module.exports = function (name, version, cb) {
+	var url = registryUrl(name.split('/')[0]);
+
 	if (typeof version !== 'string') {
 		cb = version;
 		version = '';
 	}
 
-	got(registryUrl + encodeURIComponent(name) + '/' + version, function (err, data) {
+	got(url + encodeURIComponent(name) + '/' + version, function (err, data) {
 		if (err && err.code === 404) {
 			cb(new Error('Package or version doesn\'t exist'));
 			return;
