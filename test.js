282119311c97b5e7ebd9f61372635f61cfb5fce3
Sindre Sorhus <sindresorhus@gmail.com>
2017-03-26 17:36:35 +0700

    Breaking changes
    - The `version` (second) argument is now an options object. Specify the `version` option instead.
    - Return an abbreviated metadata by default. Use the `fullMetadata` option to get back the previous behavior.
    - Return the latest version by default instead of all versions. Use the `allVersions` option to get back the previous behavior.
    
    Fixes #32

--- test.js
+++ test.js
@@ -3,54 +3,73 @@ import test from 'ava';
 import privateRegistry from 'mock-private-registry/promise';
 import m from '.';
 
-test('full', async t => {
-	const json = await m('pageres');
-	t.is(json.name, 'pageres');
-	t.is(json._id, 'pageres');
+test('latest version', async t => {
+	const json = await m('ava');
+	t.is(json.name, 'ava');
+	t.falsy(json.versions);
 });
 
-test('latest version', async t => {
-	const json = await m('pageres', 'latest');
+test('full metadata', async t => {
+	const json = await m('pageres', {
+		fullMetadata: true,
+		version: '4.4.0'
+	});
 	t.is(json.name, 'pageres');
+	t.is(json._id, 'pageres@4.4.0');
+});
+
+test('all version', async t => {
+	const json = await m('pageres', {allVersions: true});
+	t.is(json.name, 'pageres');
+	t.is(json.versions['0.1.0'].name, 'pageres');
 });
 
 test('specific version', async t => {
-	const json = await m('pageres', '0.1.0');
+	const json = await m('pageres', {version: '0.1.0'});
 	t.is(json.version, '0.1.0');
 });
 
 test('incomplete version x', async t => {
-	const json = await m('pageres', '0');
+	const json = await m('pageres', {version: '0'});
 	t.is(json.version.substr(0, 2), '0.');
 });
 
-test('scoped - full', async t => {
+test('scoped - latest version', async t => {
 	const json = await m('@sindresorhus/df');
 	t.is(json.name, '@sindresorhus/df');
-	t.is(json._id, '@sindresorhus/df');
 });
 
-test('scoped - latest version', async t => {
-	const json = await m('@sindresorhus/df', 'latest');
+test('scoped - full metadata', async t => {
+	const json = await m('@sindresorhus/df', {
+		fullMetadata: true,
+		version: '1.0.1'
+	});
 	t.is(json.name, '@sindresorhus/df');
+	t.is(json._id, '@sindresorhus/df@1.0.1');
+});
+
+test('scoped - all version', async t => {
+	const json = await m('@sindresorhus/df', {allVersions: true});
+	t.is(json.name, '@sindresorhus/df');
+	t.is(json.versions['1.0.1'].name, '@sindresorhus/df');
 });
 
 test('scoped - specific version', async t => {
-	const json = await m('@sindresorhus/df', '1.0.1');
+	const json = await m('@sindresorhus/df', {version: '1.0.1'});
 	t.is(json.version, '1.0.1');
 });
 
 test('scoped - dist tag', async t => {
-	const json = await m('@rexxars/npmtest', 'next');
+	const json = await m('@rexxars/npmtest', {version: 'next'});
 	t.is(json.version, '2.0.0');
 });
 
 test('reject when version doesn\'t exist', async t => {
-	t.throws(m('hapi', '6.6.6'), 'Version doesn\'t exist');
+	await t.throws(m('hapi', {version: '6.6.6'}), 'Version doesn\'t exist');
 });
 
 test('reject when package doesn\'t exist', async t => {
-	t.throws(m('nnnope'), 'Package `nnnope` doesn\'t exist');
+	await t.throws(m('nnnope'), 'Package `nnnope` doesn\'t exist');
 });
 
 test.cb('does not send any auth token for unconfigured registries', t => {
@@ -59,7 +78,7 @@ test.cb('does not send any auth token for unconfigured registries', t => {
 	});
 
 	server.listen(63144, '127.0.0.1', async () => {
-		const json = await m('@mockscope3/foobar');
+		const json = await m('@mockscope3/foobar', {allVersions: true});
 		t.is(json.headers.host, 'localhost:63144');
 		t.is(json.headers.authorization, undefined);
 		server.close(t.end);
@@ -68,7 +87,7 @@ test.cb('does not send any auth token for unconfigured registries', t => {
 
 test('private registry (bearer token)', async t => {
 	const server = await privateRegistry();
-	const json = await m('@mockscope/foobar', 'latest');
+	const json = await m('@mockscope/foobar');
 	t.is(json.name, '@mockscope/foobar');
 	server.close();
 });
@@ -80,7 +99,7 @@ test('private registry (basic token)', async t => {
 		token: 'QWxhZGRpbjpPcGVuU2VzYW1l',
 		tokenType: 'Basic'
 	});
-	const json = await m('@mockscope2/foobar', 'latest');
+	const json = await m('@mockscope2/foobar');
 	t.is(json.name, '@mockscope2/foobar');
 	server.close();
 });
