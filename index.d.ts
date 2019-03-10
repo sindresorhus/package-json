9156854020ccd13b0baa91d4245b86e270c65a1f
Dimitri Benin <dimitri.benin@gmail.com>
2019-03-10 15:47:48 +0100

    Improve types for objects returned npm API, differentiate between full/abbreviated metadata

--- index.d.ts
+++ index.d.ts
@@ -27,35 +27,97 @@ export interface Options {
 	readonly allVersions?: boolean;
 }
 
-export interface PackageJson {
-	readonly [k: string]: unknown;
-	readonly name?: string;
-	readonly version?: string;
-	readonly files?: ReadonlyArray<string>;
-	readonly bin?: {[k: string]: string};
-	readonly man?: ReadonlyArray<string>;
-	readonly keywords?: ReadonlyArray<string>;
-	readonly author?: Person;
-	readonly maintainers?: ReadonlyArray<Person>;
-	readonly contributors?: ReadonlyArray<Person>;
-	readonly bundleDependencies?: {[name: string]: string};
-	readonly dependencies?: {[name: string]: string};
-	readonly devDependencies?: {[name: string]: string};
-	readonly optionalDependencies?: {[name: string]: string};
-	readonly description?: string;
-	readonly engines?: {[type: string]: string};
-	readonly license?: string;
-	readonly repository?: {type: string; url: string};
-	readonly bugs?: {url: string; email?: string} | {url?: string; email: string};
-	readonly homepage?: string;
-	readonly scripts?: {[k: string]: string};
-	readonly readme?: string;
+export interface FullMetadataOptions extends Options {
+	/**
+	 * By default, only an abbreviated metadata object is returned for performance reasons. [Read more.](https://github.com/npm/registry/blob/master/docs/responses/package-metadata.md)
+	 *
+	 * @default false
+	 */
+	readonly fullMetadata: true;
+}
+
+export interface DistTags {
+	readonly latest: string;
+	readonly [tagName: string]: string;
+}
+
+export interface AbbreviatedMetadata {
+	readonly 'dist-tags': DistTags;
+	readonly modified: string;
+	readonly name: string;
+	readonly versions: {readonly [version: string]: AbbreviatedVersion};
+	readonly [key: string]: unknown;
+}
+
+export interface AbbreviatedVersion {
+	readonly name: string;
+	readonly version: string;
+	readonly dist: {
+		readonly shasum: string;
+		readonly tarball: string;
+		readonly integrity?: string;
+	};
+	readonly deprecated?: string;
+	readonly dependencies?: {readonly [name: string]: string};
+	readonly optionalDependencies?: {readonly [name: string]: string};
+	readonly devDependencies?: {readonly [name: string]: string};
+	readonly bundleDependencies?: {readonly [name: string]: string};
+	readonly peerDependencies?: {readonly [name: string]: string};
+	readonly bin?: {readonly [key: string]: string};
+	readonly directories?: ReadonlyArray<string>;
+	readonly engines?: {readonly [type: string]: string};
+	readonly _hasShrinkwrap?: boolean;
+	readonly [key: string]: unknown;
 }
 
 export interface Person {
 	readonly name?: string;
 	readonly email?: string;
 	readonly url?: string;
+}
+
+export interface HoistedData {
+	readonly author?: Person;
+	readonly bugs?:
+		| {readonly url: string; readonly email?: string}
+		| {readonly url?: string; readonly email: string};
+	readonly contributors?: ReadonlyArray<Person>;
+	readonly description?: string;
+	readonly homepage?: string;
+	readonly keywords?: ReadonlyArray<string>;
+	readonly license?: string;
+	readonly maintainers?: ReadonlyArray<Person>;
+	readonly readme?: string;
+	readonly readmeFilename?: string;
+	readonly repository?: {readonly type: string; readonly url: string};
+}
+
+export interface FullMetadata extends AbbreviatedMetadata, HoistedData {
+	readonly _id: string;
+	readonly _rev: string;
+	readonly time: {
+		readonly created: string;
+		readonly modified: string;
+		readonly [version: string]: string;
+	};
+	readonly users?: {readonly [user: string]: boolean};
+	readonly versions: {readonly [version: string]: FullVersion};
+	readonly [key: string]: unknown;
+}
+
+export interface FullVersion extends AbbreviatedVersion, HoistedData {
+	readonly _id: string;
+	readonly _nodeVersion: string;
+	readonly _npmUser: string;
+	readonly _npmVersion: string;
+	readonly main?: string;
+	readonly files?: ReadonlyArray<string>;
+	readonly man?: ReadonlyArray<string>;
+	readonly scripts?: {readonly [scriptName: string]: string};
+	readonly gitHead?: string;
+	readonly types?: string;
+	readonly typings?: string;
+	readonly [key: string]: unknown;
 }
 
 /**
@@ -65,8 +127,12 @@ export interface Person {
  */
 export default function packageJson(
 	name: string,
+	options: FullMetadataOptions
+): Promise<FullMetadata>;
+export default function packageJson(
+	name: string,
 	options?: Options
-): Promise<PackageJson>;
+): Promise<AbbreviatedMetadata>;
 
 /**
  * The error thrown when the given package name cannot be found.
