73167933b8bb7dbb15d216903d10be06cf3958f5
tommy-mitchell <tmitchell7@uh.edu>
2024-02-20 13:30:08 -0600

    fix: use correct option

--- index.js
+++ index.js
@@ -32,7 +32,7 @@ export class VersionNotFoundError extends Error {
 export default async function packageJson(packageName, options) {
 	options = {
 		version: 'latest',
-		omitDeprecated: true,
+		ignoreDeprecated: true,
 		...options,
 	};
 
@@ -90,7 +90,7 @@ export default async function packageJson(packageName, options) {
 	} else if (version) {
 		const versionExists = Boolean(data.versions[version]);
 
-		if (options.omitDeprecated && !versionExists) {
+		if (options.ignoreDeprecated && !versionExists) {
 			for (const [metadataVersion, metadata] of Object.entries(data.versions)) {
 				if (metadata.deprecated) {
 					delete data.versions[metadataVersion];
