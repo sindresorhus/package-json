74d4b8e5429bd43ac5e17e711fc8acdb2418a6e0
tommy-mitchell <tmitchell7@uh.edu>
2024-02-22 12:47:06 -0600

    update type tests

--- index.test-d.ts
+++ index.test-d.ts
@@ -1,4 +1,4 @@
-import {expectType} from 'tsd';
+import {expectAssignable, expectError} from 'tsd';
 import packageJson, {
 	type FullMetadata,
 	type FullVersion,
@@ -8,29 +8,43 @@ import packageJson, {
 	VersionNotFoundError,
 } from './index.js';
 
-expectType<Promise<AbbreviatedMetadata>>(packageJson('package-json'));
-expectType<Promise<AbbreviatedMetadata>>(
+expectAssignable<Promise<AbbreviatedVersion>>(packageJson('package-json'));
+expectAssignable<Promise<AbbreviatedVersion>>(
 	packageJson('package-json', {version: '1.2.3'}),
 );
-expectType<Promise<AbbreviatedMetadata>>(
+expectAssignable<Promise<AbbreviatedMetadata>>(
 	packageJson('package-json', {allVersions: true}),
 );
-expectType<Promise<FullMetadata>>(
+expectAssignable<Promise<FullVersion>>(
 	packageJson('package-json', {fullMetadata: true}),
+);
+expectAssignable<Promise<FullMetadata>>(
+	packageJson('package-json', {fullMetadata: true, allVersions: true}),
 );
 
 const abbreviatedMetadata = await packageJson('package-json');
-expectType<AbbreviatedVersion>(abbreviatedMetadata.versions['1.2.3']);
-const fullMetadata = await packageJson('package-json', {fullMetadata: true});
-expectType<FullVersion>(fullMetadata.versions['1.2.3']);
+expectAssignable<string>(abbreviatedMetadata.version);
+expectError(abbreviatedMetadata.versions);
 
-expectType<typeof PackageNotFoundError>(PackageNotFoundError);
-expectType<typeof VersionNotFoundError>(VersionNotFoundError);
+const fullMetadata = await packageJson('package-json', {fullMetadata: true});
+expectAssignable<string>(fullMetadata.version);
+expectError(fullMetadata.versions);
+
+const abbreviatedVersions = await packageJson('package-json', {allVersions: true});
+expectAssignable<AbbreviatedVersion | undefined>(abbreviatedVersions.versions['1.2.3']);
+expectError(abbreviatedVersions.version);
+
+const fullVersions = await packageJson('package-json', {fullMetadata: true, allVersions: true});
+expectAssignable<FullVersion | undefined>(fullVersions.versions['1.2.3']);
+expectError(fullVersions.version);
+
+expectAssignable<typeof PackageNotFoundError>(PackageNotFoundError);
+expectAssignable<typeof VersionNotFoundError>(VersionNotFoundError);
 
 const packageNotFoundError = new PackageNotFoundError('foo');
 packageNotFoundError instanceof PackageNotFoundError; // eslint-disable-line @typescript-eslint/no-unused-expressions
-expectType<PackageNotFoundError>(packageNotFoundError);
+expectAssignable<PackageNotFoundError>(packageNotFoundError);
 
 const versionNotFoundError = new VersionNotFoundError('foo', 'bar');
 versionNotFoundError instanceof VersionNotFoundError; // eslint-disable-line @typescript-eslint/no-unused-expressions
-expectType<VersionNotFoundError>(versionNotFoundError);
+expectAssignable<VersionNotFoundError>(versionNotFoundError);
